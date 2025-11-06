df293d7b6c06baa40317a43bcc81f1a7622ddb65
unknown <aluno@UNA01DT00402156.animaedu.intranet>
2025-11-06 20:09:05 -0300

    Workflow de teste atualizado

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,18 @@
 def test_soma():
     assert 1 + 1 == 2
+
+def test_subtracao():
+    assert 5 - 3 == 2
+
+def test_string_maiuscula():
+    assert "hello".upper() == "HELLO"
+
+def test_tamanho_lista():
+    minha_lista = [1, "a", True]
+    assert len(minha_lista) == 3
+
+def test_mod():
+    val = 18
+    val2 = 10
+
+    assert 18 % 10 == 8
